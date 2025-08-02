275b275802196770cea7521987f13d0ff84aae73
Henrique Valvassori <henriquevalvassori41@gmail.com>
2025-08-02 00:31:43 -0300

    atualizacao de app

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+{
+    "name": "MotivacionalAI",
+    "short_name": "MotivacionalAI",
+    "description": "Inspiração com vídeos e frases motivacionais, dicas de treino, últimas notícias de IA e ferramentas para PDF, imagens e documentos. Alcance seu potencial!",
+    "start_url": "./",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#212529",
+    "icons": [
+      {
+        "src": "img/channels4_profile.jpg",
+        "sizes": "192x192",
+        "type": "image/jpg",
+        "purpose": "any maskable"
+      },
+      {
+        "src": "img/channels4_profile.jpg",
+        "sizes": "512x512",
+        "type": "image/jpg",
+        "purpose": "any maskable"
+      }
+    ]
+  }
